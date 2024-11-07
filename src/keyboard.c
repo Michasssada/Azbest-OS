0393c0edc9d559f344be1d4613fd7f94665a3433
Michasssada <michaslate@gmail.com>
2024-11-07 22:38:30 +0100

    more cleaning

--- src/keyboard.c
+++ src/keyboard.c
@@ -7,6 +7,7 @@
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
 char input_char[256];
+size_t input_len = 0;
 static const char scancode_to_char[] = {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8',    /* 0x00 - 0x09 */
     '9', '0', '-', '=', '\b', /* Backspace */
@@ -27,15 +28,17 @@ static const char scancode_to_char[] = {
 static inline void outw(uint16_t port, uint16_t val) {
     __asm__ volatile ("outw %0, %1" : : "a"(val), "Nd"(port));
 }
+
 static inline uint8_t inb(uint16_t port) {
     uint8_t result;
     __asm__ volatile ("inb %1, %0" : "=a"(result) : "Nd"(port));
     return result;
 }
+
 static inline void outb(uint16_t port, uint8_t value) {
     __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
 }
-size_t input_len = 0;              // Current length of the input
+
 /* Keyboard scancode mapping (partial, for simplicity) */
 void clear_buffer(){
     input_len = 0;
