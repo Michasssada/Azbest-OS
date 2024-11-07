f6ed95267a70671fb8166eb9415a0211cf9aa1ed
Michasssada <michaslate@gmail.com>
2024-11-07 22:31:41 +0100

    idk

--- include/terminal.h
+++ include/terminal.h
@@ -1,15 +1,18 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-void terminal_writestring(const char* data);
-void terminal_write(const char* data, size_t size);
-void terminal_initialize(void);
-void terminal_setcolor(uint8_t color);
-static inline void outb(uint16_t port, uint8_t value);
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
-void terminal_putchar(char c);
-void terminal_clear();
+
 extern size_t terminal_row;
 extern size_t terminal_column;
 extern uint8_t terminal_color;
 extern bool isEnterPressed;
+
+void terminal_writestring(const char* data);
+void terminal_write(const char* data, size_t size);
+void terminal_initialize(void);
+void terminal_setcolor(uint8_t color);
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
+void terminal_putchar(char c);
+void terminal_clear();
+
+static inline void outb(uint16_t port, uint8_t value);
