5e01ca2778298ae36bf640045f0a199645c42e34
Michasssada <michaslate@gmail.com>
2024-11-11 18:04:17 +0100

    l

--- build.py
+++ build.py
@@ -26,11 +26,13 @@ def get_all_files(directory):
 
 directory_path = os.getcwd()+"/src"
 files_to_build, files_to_build_no_extensions = get_all_files(directory_path)
+
 for i in range(len(files_to_build)):
     exit_code = os.system(f"i686-elf-gcc -c {os.getcwd() + "/src/" + files_to_build[i]}  -o {"build/"+files_to_build_no_extensions[i]+".o"} -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Iinclude")
     if exit_code != 0:
         quit()
 print("system built")
+
 built_files = os.getcwd()+"/build"
 files_to_link = [os.path.join(built_files, f) for f in os.listdir(built_files) if os.path.isfile(os.path.join(built_files, f))]
 link = ' '.join([item for item in files_to_link])
@@ -38,6 +40,7 @@ exit_code = os.system(f"i686-elf-gcc -T linker.ld -o build/Azbest_OS.bin -ffrees
 if(exit_code != 0):
     quit()
 print("system linked")
+
 if args.nogrub:
     exit_code = os.system(f"mv build/Azbest_OS.bin {os.getcwd()}")
     if args.boot:
@@ -46,6 +49,7 @@ else:
     exit_code = os.system("mv build/Azbest_OS.bin iso/boot")
     if(exit_code != 0):
         quit()
+    
     print("running grub-mkrescue")
     exit_code = os.system("grub-mkrescue -o Azbest_OS.iso iso")
     if(exit_code != 0):
