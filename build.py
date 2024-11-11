48f7d4cb78a3e30a3df9691f63ed6fb0199b4d4f
Michasssada <michaslate@gmail.com>
2024-11-11 16:52:28 +0100

    build system builds the system

--- build.py
+++ build.py
@@ -1,12 +1,31 @@
 import os
 
 def get_all_files(directory):
-    files_list = []
+    files_with_extension = []
+    files_without_extension = []
+    
     for root, _, files in os.walk(directory):
         for file in files:
-            # Construct the relative file path (foldername/filename)
-            relative_path = os.path.join(root, file).replace(directory + os.sep, "")
-            files_list.append(relative_path)
-    return files_list
+            # File path with extension (including folder names)
+            file_with_extension = os.path.relpath(os.path.join(root, file), directory)
+            # Filename without extension
+            filename_without_extension = os.path.splitext(file)[0]
+            file_without_extension = filename_without_extension  # Just the file name without path
+            
+            # Add both versions to the respective lists
+            files_with_extension.append(file_with_extension)
+            files_without_extension.append(file_without_extension)
+    
+    return files_with_extension, files_without_extension
 
-files_to_build = get_all_files()
+
+directory_path = os.getcwd()+"/src"
+files_to_build, files_to_build_no_extensions = get_all_files(directory_path)
+for i in range(len(files_to_build)):
+    exit_code = os.system(f"i686-elf-gcc -c {os.getcwd() + "/src/" + files_to_build[i]}  -o {"build/"+files_to_build_no_extensions[i]+".o"} -std=gnu99 -ffreestanding -O2 -Wall -Wextra -L/include")
+    if exit_code != 0:
+        quit()
+print("system built")
+built_files = os.getcwd()+"/src"
+file_names = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+exit_code = os.system(f"i686-elf-gcc -T linker.ld -o build/Azbest_OS.bin -ffreestanding -O2 -nostdlib asm/boot.o {}  -lgcc -L/include")
