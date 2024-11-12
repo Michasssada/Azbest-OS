6111a85e52f30774c2fec851b3c19485ffb55dd6
micha <michaslate@gmail.com>
2024-11-12 15:39:34 +0100

    tweaks

--- build.py
+++ build.py
@@ -2,11 +2,29 @@
 import os
 import argparse
 import platform
-
+import shutil
 
 parser = argparse.ArgumentParser(description="Example of adding custom flags.")
 parser.add_argument('-b', '--boot', action='store_true', help="boots the os.")
 parser.add_argument('-g', '--nogrub', action='store_true', help="compiles only, doesn't run grub-mkrescue.")
+
+def clear_folder(folder_path):
+    # Check if the folder exists
+    if os.path.exists(folder_path):
+        # Delete all files and subdirectories in the folder
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)  # Remove file or symlink
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)  # Remove directory
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
+    else:
+        print("Folder does not exist.")
+
+clear_folder("build")
 args = parser.parse_args()
 def get_all_files(directory):
     files_with_extension = []
