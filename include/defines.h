ff14d4340b52733c487f3efdf5fd8e84e913dc09
Aktywny <axrl7744@gmail.com>
2024-11-12 19:31:02 +0100

    better

--- include/defines.h
+++ include/defines.h
@@ -1,3 +1,3 @@
-#define KEYBOARD_DELAY_CLICK 450000 //for qemu
+#define KEYBOARD_DELAY_CLICK 435000 //for qemu
 //#define KEYBOARD_DELAY_CLICK 300000 //for vm or real machine 
 #define OS_VERSION "0.0.1 alpha 2" //version
