c13f139138f7b85899b4994aadf5615ebb9c5118
Michasssada <michaslate@gmail.com>
2024-11-15 22:20:44 +0100

    cleaned

--- src/kernel.c
+++ src/kernel.c
@@ -8,19 +8,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-
-static inline void outw(uint16_t port, uint16_t val) {
-    __asm__ volatile ("outw %0, %1" : : "a"(val), "Nd"(port));
-}
-static inline uint8_t inb(uint16_t port) {
-    uint8_t result;
-    __asm__ volatile ("inb %1, %0" : "=a"(result) : "Nd"(port));
-    return result;
-}
-void default_exception_handler(){
-    // Simple infinite loop to halt on exception
-        while(1);
-}
+#include "stdlib/string.h"
 void init(){
     initGdt();
     init_heap();
