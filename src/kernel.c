cd07fa889a04842bf776f497b5777dadd0fddaad
Michasssada <michaslate@gmail.com>
2024-12-24 14:48:24 +0100

    modified:   src/kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -24,12 +24,9 @@ void kmain(uint32_t magic, struct multiboot_info* bootInfo)
     terminal_writestring("Welcome to Azbest OS! ver:"OS_VERSION". No rights reserved hehe\n");
     terminal_setcolor(10);
     terminal_writestring("> ");
-
     uint32_t mod1 = *(uint32_t*)(bootInfo->mods_addr + 4);
     uint32_t physicalAllocStart = (mod1 + 0xFFF) & ~0xFFF;
-
     initMemory(bootInfo->mem_upper * 1024, physicalAllocStart);
-
     initKeyboard();
 	while(1){
 	}
